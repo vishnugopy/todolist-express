7ba2ea69d5e22ae2167c943b29361a95e6df4b50
Vishnu Gopy <vishnugoby0010@gmail.com>
2021-03-22 22:07:45 +0100

    todo basics 1h followed

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 
-const { request, response } = require("express");
 const express = require("express");
+const ejs = require("ejs");
+const router = require("./routers")
 
 
 const server = express();
 
 
-server.get("/" , (request , response)=>{
-    response.send("ca fonction")
-})
+server.engine("ejs" , ejs.renderFile);
+server.set("views" , "./src/views")
+
+
+server.use(router)
 
 server.listen(1010 , () => {
     console.log("still runing me");
